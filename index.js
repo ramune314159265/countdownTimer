dbf21d6390bbc7d4503d26cb8250e48259c0d0b7
BuildTools <unconfigured@null.spigotmc.org>
2023-12-10 23:14:42 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const url = new URL(location.href)
+
+const countdownTitle = url.searchParams.get('title')
+const unixTime = url.searchParams.get('unixtime')
+const date = new Date(unixTime * 1000)
+
+const countDownHandle = () => {
+    const timeDiff = date.getTime() - new Date().getTime()
+
+    const hour = (Math.floor(timeDiff / 1000 / 60 / 60)).toString().padStart(2, '0')
+    const min = (Math.floor(timeDiff / 1000 / 60) % 60).toString().padStart(2, '0')
+    const sec = (Math.floor(timeDiff / 1000) % 60).toString().padStart(2, '0')
+
+    document.querySelector('#countdownHours2').textContent = hour[0]
+    document.querySelector('#countdownHours1').textContent = hour[1]
+    document.querySelector('#countdownMinutes2').textContent = min[0]
+    document.querySelector('#countdownMinutes1').textContent = min[1]
+    document.querySelector('#countdownSeconds2').textContent = sec[0]
+    document.querySelector('#countdownSeconds1').textContent = sec[1]
+
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+        document.querySelector('.countdownTitle').textContent = countdownTitle
+        setInterval(countDownHandle, 1000);
+        countDownHandle()
+    }, 1000 - new Date().getMilliseconds());
+})
