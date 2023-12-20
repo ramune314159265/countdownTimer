69591fc2b7c9df45dde54ef42acd5e17e911c79b
BuildTools <unconfigured@null.spigotmc.org>
2023-12-20 19:26:50 +0900

    #2 対応

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const url = new URL(location.href)
 
-const countdownTitle = url.searchParams.get('title')
+const countdownTitles = {
+    before: url.searchParams.get('titlebefore'),
+    then: url.searchParams.get('titlethen'),
+    after: url.searchParams.get('titleafter'),
+}
 const unixTime = url.searchParams.get('unixtime')
 const date = new Date(unixTime * 1000)
 
 const countDownHandle = () => {
-    const timeDiff = Math.round(Math.abs(date.getTime() - Date.now()) / 1000) * 1000
+    const timeDiff = Math.round((date.getTime() - Date.now()) / 1000)
+    const timeDiffAbsolutize = Math.abs(timeDiff)
 
-    const hour = (Math.floor(timeDiff / 1000 / 60 / 60)).toString().padStart(2, '0')
-    const min = (Math.floor(timeDiff / 1000 / 60) % 60).toString().padStart(2, '0')
-    const sec = (Math.floor(timeDiff / 1000) % 60).toString().padStart(2, '0')
+    console.log(timeDiff)
+
+    const hour = (Math.floor(timeDiffAbsolutize / 60 / 60)).toString().padStart(2, '0')
+    const min = (Math.floor(timeDiffAbsolutize / 60) % 60).toString().padStart(2, '0')
+    const sec = (Math.floor(timeDiffAbsolutize) % 60).toString().padStart(2, '0')
 
     document.querySelector('#countdownHours2').textContent = hour[0]
     document.querySelector('#countdownHours1').textContent = hour[1]
@@ -18,10 +25,23 @@ const countDownHandle = () => {
     document.querySelector('#countdownSeconds2').textContent = sec[0]
     document.querySelector('#countdownSeconds1').textContent = sec[1]
 
+    switch (true) {
+        case (timeDiff > 0):
+            document.querySelector('.countdownTitle').textContent = countdownTitles.before
+            break
+        case (timeDiff === 0):
+            document.querySelector('.countdownTitle').textContent = countdownTitles.then
+            break
+        case (timeDiff < 0):
+            document.querySelector('.countdownTitle').textContent = countdownTitles.after
+            break
+        default:
+            break
+    }
+
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('.countdownTitle').textContent = countdownTitle
     countDownHandle()
     setTimeout(() => {
         setInterval(countDownHandle, 1000);
